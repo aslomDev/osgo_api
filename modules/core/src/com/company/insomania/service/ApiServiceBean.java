81c2b8640b820d9b16757d7e991c7f5c7d36a914
aslomDev <aslomphp@gmail.com>
2021-01-05 15:14:58 +0500

    basic Auth

--- modules/core/src/com/company/insomania/service/ApiServiceBean.java
+++ modules/core/src/com/company/insomania/service/ApiServiceBean.java
@@ -8,7 +8,13 @@ import com.haulmont.cuba.core.Transaction;
 import com.haulmont.cuba.core.TypedQuery;
 import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.global.TimeSource;
+import jdk.internal.jline.internal.Urls;
+import org.apache.http.Header;
+import org.apache.http.entity.ContentType;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -39,58 +45,41 @@ public class ApiServiceBean implements ApiService {
 
     @Override
     public RequestResult sendToProvider() throws NoSuchAlgorithmException, KeyManagementException {
-        //sertificat
-//        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
-//            public X509Certificate[] getAcceptedIssuers() { return null; }
-//            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-//            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
-//        }
-//        };
-
-//        //ssl
-//        SSLContext sc = SSLContext.getInstance("SSL");
-//        sc.init(null, trustAllCerts, new SecureRandom());
-//        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-        // Create all-trusting host name verifier
-//        HostnameVerifier allHostsValid = (hostname, session) -> true;
-//
-//        // Install the all-trusting host verifier
-//        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
         RequestResult requestResult = new RequestResult();
         try {
-//            String orl = "api.e-osgo.uz";
             URL targetUrl = null;
-            String user = "";
-            String password = "";
-            String grant_type = "";
+            String USERNAME = "";
+            String PASSWORD = "";
+            String grant_type = "password";
+            HttpHeaders httpHeaders = new HttpHeaders();
+
             List<Settings> settings = getSettings();
             for (Settings setting : settings) {
                 if (setting.getName().equals("url")) targetUrl = new URL(setting.getValue());
-                if (setting.getName().equals("user")) user = setting.getValue();
-                if (setting.getName().equals("password")) password = setting.getValue();
-//                if (setting.getName().equals("grant_type")) grant_type = setting.getValue();
+                if (setting.getName().equals("USERNAME")) USERNAME = setting.getValue();
+                if (setting.getName().equals("PASSWORD")) PASSWORD = setting.getValue();
             }
             String basicAuth = "Basic " + Base64.getEncoder().encodeToString(
-                    (user+":"+password).getBytes(StandardCharsets.UTF_8));
+                    (USERNAME+":"+PASSWORD).getBytes(StandardCharsets.UTF_8));
+
+
+
+
             HttpURLConnection httpConnection =
                     (HttpURLConnection) Objects.requireNonNull(targetUrl).openConnection();
             httpConnection.setDoOutput(true);
-//            httpConnection.setRequestProperty("Content-Type", "application/json");
+            httpConnection.setDoInput(true);
             httpConnection.setRequestMethod("GET");
-//            httpConnection. connect();
-//            httpConnection.setRequestMethod(method.getRequestType().getId());
             httpConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
             httpConnection.setRequestProperty("Accept", "application/json");
-            httpConnection.setRequestProperty("Authorization", basicAuth);
-            httpConnection.setRequestProperty("X-Auth", basicAuth);
+            httpConnection.setRequestProperty("Authorization: " + "Basic ", basicAuth);
 
 
-//            JSONObject jo = new JSONObject(createrequest());
-//            jo.put("id", 1);
-//            request = jo.toString(4);
+            System.out.println(httpConnection);
+
+            //// https://stackoverflow.com/questions/3283234/http-basic-authentication-in-java-using-httpclient
+
             OutputStream outputStream = httpConnection.getOutputStream();
-//            outputStream.write(createrequest().getBytes());
             outputStream.flush();
 
             if (httpConnection.getResponseCode() != 200) {
@@ -113,37 +102,6 @@ public class ApiServiceBean implements ApiService {
             }
             httpConnection.disconnect();
 
-//            httpConnection.setRequestMethod(auths(user, password, grant_type));
-//            httpConnection.setRequestMethod();
-//
-//            JSONObject jo = new JSONObject(request);
-//            jo.put("id", gateTransactionId);
-//            request = jo.toString(4);
-//            OutputStream outputStream = httpConnection.getOutputStream();
-//            outputStream.write(request.getBytes());
-//            outputStream.flush();
-
-//            if (httpConnection.getResponseCode() != 200) {
-//                InputStreamReader inputStreamReader = new InputStreamReader((httpConnection.getErrorStream()));
-//                BufferedReader responseBuffer = new BufferedReader(inputStreamReader);
-//                StringBuilder res = new StringBuilder();
-//                String output;
-//                while ((output = responseBuffer.readLine()) != null) {
-//                    res.append(output).append("\n");
-//                    System.out.println(output);
-//                }
-//                transact(gateTransactionId, method, request, httpConnection.getResponseCode(), res.toString());
-//                requestResult.setResponseCode(httpConnection.getResponseCode());
-//                requestResult.setResponse(res.toString());
-//            } else {
-//                InputStreamReader inputStreamReader = new InputStreamReader((httpConnection.getInputStream()));
-//                BufferedReader responseBuffer = new BufferedReader(inputStreamReader);
-//                String res = String.valueOf((new JSONObject(responseBuffer.readLine()).toString(4)));
-//                transact(gateTransactionId, method, request, httpConnection.getResponseCode(), res);
-//                requestResult.setResponseCode(httpConnection.getResponseCode());
-//                requestResult.setResponse(res);
-//            }
-//            httpConnection.disconnect();
         } catch (IOException e) {
             e.printStackTrace();
         }
